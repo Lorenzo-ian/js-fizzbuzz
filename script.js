3d603978d82d930401678a6ab283126f85d72c2d
Lorenzo-ian <lorenzoiannini96@gmail.com>
2019-07-07 11:18:30 +0200

    correzione

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  e per i multipli di 5 stampi Buzz Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”*/
 
  for (i= 1; i <= 100; i++){
-    if( i % 15 === 0){
+    if( i % 3 === 0 && i % 5 === 0){
       console.log("FizzBuzz");
     } else if (i % 5 === 0){
       console.log("Buzz");
