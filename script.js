a4c7b1d059a02fd44c1860aa6b1883ac8dfb383f
Lorenzo-ian <lorenzoiannini96@gmail.com>
2019-07-07 11:23:17 +0200

    Aggiungo commenti

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
  e per i multipli di 5 stampi Buzz Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”*/
 
  for (i= 1; i <= 100; i++){
+   //Stampo fizz buzz con && unendo due condizioni (3 e 5 )
     if( i % 3 === 0 && i % 5 === 0){
       console.log("FizzBuzz");
+      //Stampo la condizione con 5
     } else if (i % 5 === 0){
       console.log("Buzz");
+      //Stampo la condizione con 3
     } else if (i % 3 === 0 ){
       console.log("Fizz");
+      //Stampo unendo tutte le condizioni
     } else{
       console.log(i);
     }
