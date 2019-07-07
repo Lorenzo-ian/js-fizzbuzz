3c46d3278329db829ef62b6a1a08279bd35a8455
Lorenzo-ian <lorenzoiannini96@gmail.com>
2019-07-07 12:51:44 +0200

    aggiunta lista

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 /* Scrivi un programma che stampi i numeri da 1 a 100 ma per i multipli di 3 stampi “Fizz” al posto del numero
  e per i multipli di 5 stampi Buzz Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”*/
 
- for (i= 1; i <= 100; i++){
-   //Stampo fizz buzz con && unendo due condizioni (3 e 5 )
-    if( i % 3 === 0 && i % 5 === 0){
-      console.log("FizzBuzz");
-      //Stampo la condizione con 5
-    } else if (i % 5 === 0){
-      console.log("Buzz");
-      //Stampo la condizione con 3
-    } else if (i % 3 === 0 ){
-      console.log("Fizz");
-      //Stampo unendo tutte le condizioni
-    } else{
-      console.log(i);
-    }
+var elenco, elencotemp;
+elenco = document.getElementById("mio_id");
+console.log(elenco);
+
+for ( var i = 1; i < 101; i++) {
+  elencotemp = elenco.innerHTML;
+  console.log("valore elencotemp" + i + elencotemp)
+  //Stampo fizz buzz con && unendo due condizioni (3 e 5 )
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    elenco.innerHTML = elencotemp + "<li>" + "fizzbuzz" + "</li>";
+    //Stampo la condizione con 5
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+    elenco.innerHTML = elencotemp + "<li>" + "buzz" + "</li>";
+    //Stampo la condizione con 3
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+    elenco.innerHTML = elencotemp + "<li>" + "fizz" + "</li>";
+    //Stampo unendo tutte le condizioni
+  } else {
+    console.log(i);
+    elenco.innerHTML = elencotemp + "<li>" + i + "</li>";
   }
+}
